e8e09258b6ff0e56928e3ec7dc6fe42309bc0222
eliiimk <144788146+eliiimk@users.noreply.github.com>
2024-11-14 22:58:17 +0100

    Delete locations.py

--- locations.py
+++
@@ -1,6 +0,0 @@
-map = {
-    "start": "Vous êtes au début de votre aventure, dans une clairière calme.",
-    "forest1": "Vous marchez dans une forêt dense.",
-    "forest2": "Les arbres autour de vous sont plus sombres et menaçants.",
-    "boss": "Vous êtes devant une grande clairière, le boss se trouve ici !"
-}
